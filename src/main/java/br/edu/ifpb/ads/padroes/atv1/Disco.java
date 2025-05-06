d73f9ab7e26e22272fc69612f24f787af5ae1650
Diogo Moreira <diogo.dmoreira@gmail.com>
2025-05-05 22:48:32 -0300

    Commit inicial

---
+++ src/main/java/br/edu/ifpb/ads/padroes/atv1/Disco.java
@@ -0,0 +1,48 @@
+package br.edu.ifpb.ads.padroes.atv1;
+
+public class Disco {
+
+    private String artista;
+    private String titulo;
+    private String genero;
+    private int anoLancamento;
+
+    public Disco(String artista, String titulo, String genero, int anoLancamento) {
+        this.artista = artista;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+}
